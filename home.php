66b8e5c6ec4b3e57a6d58be0f41090200d31793a
ChangHao <1710121095@vip.henu.edu.cn>
2018-10-03 17:11:39 +0800

    ch first commit

---
+++ home.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2018/10/1
+ * Time: 23:43
+ */
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>team6笔记</title>
+
+    <link rel="stylesheet" type="text/css" href="css/home.css">
+    <script>
+        function change2new(obj){
+            var middle1 = document.getElementById("middle1");
+            var middle2 = document.getElementById("middle2");
+            if(obj.id === 'new'){
+                middle1.style.display = "none";
+                middle2.style.display = "block";
+            } else {
+                middle1.style.display = "block";
+                middle2.style.display = "none";
+            }
+        }
+    </script>
+</head>
+<body>
+<div id="left"><?php include("left.php")?>  </div>
+<div id="middle">
+    <div id="middle1"><?php include("star.php") ?></div>
+    <div id="middle2"><?php include("notes.php") ?></div>
+</div>
+<div id="edit"><?php include ("edit.php")?></div>
+
+</body>
+</html>
---
+++ notes.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2018/10/2
+ * Time: 15:00
+ */
+?>
+
+
+</<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+</head>
+<body>
+    中间1
+</body>
+</html>
