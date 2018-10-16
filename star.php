24edb0e0530aacfa4979d9d6706f2c9b54ba153c
ChangHao <1710121095@vip.henu.edu.cn>
2018-10-16 23:24:18 +0800

    adjust some css files

--- star.php
+++ star.php
@@ -12,12 +12,17 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <!-- <link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://cdn.bootcss.com/jquery/2.1.1/jquery.min.js"></script>
-    <script src="https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> -->
+    <script src="https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="css/stars.css" type="text/css">
 
 <!-- 不能重复引用,主页面的时候删掉 -->
+    <script>
+        $(document).ready(function () {
+            
+        });
+    </script>
 </head>
 
 <body>
@@ -27,13 +32,14 @@
         </div>
         <div class='clear'></div>
         <hr/>
-        <div id="stars_list" style="padding = 5px 10px;">
-            <div id="star_single">
-            <span class="glyphicon glyphicon-book pull-left" style="color: rgb(51, 41, 47);">&nbsp</span>
-            <div id="star_name" class="pull-left">xm1d</div>
-            <span class="glyphicon glyphicon-remove-circle pull-right" style="color: rgb(51, 41, 47);"></span>
+    </div>
+    <div id="stars_list">
+            <div id="starid_1" class="star_single">
+                <span class="glyphicon glyphicon-book pull-left">&nbsp</span>
+                <div id="star_name" class="pull-left">xm1d</div>
+                <span class="glyphicon glyphicon-remove-circle pull-right"></span>
+                <div class="clear"></div>
             </div>
         </div>
-    </div>
 </body>
 </html>
